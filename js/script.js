00e8f2e5c0b21cf48459c3f15f822f719bc576fe
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-07-26 14:15:46 +0300

    toggle function added in jquery,  cursor added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-    
+    $('#design').click(function () {
+      $('.des_p').toggle();
+      $('#design').toggle();
+    });
+    $('#dev').click(function () {
+      $('#dev').toggle();
+      $('.dev_p').toggle();
+    });
+    $('#prod').click(function () {
+      $('#prod').toggle();
+      $('.prod_p').toggle();
+    })
 });
